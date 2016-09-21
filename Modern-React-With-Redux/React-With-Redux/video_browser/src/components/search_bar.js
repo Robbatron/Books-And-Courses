0348e3d4d2d46f35e9af046b79bf41a0ead742b7
Robbie Delfs <robertdelfs@Roberts-MacBook-Pro.local>
2016-09-21 01:08:01 -0700

    bedtime zzz

--- Modern-React-With-Redux/React-With-Redux/video_browser/src/components/search_bar.js
+++ Modern-React-With-Redux/React-With-Redux/video_browser/src/components/search_bar.js
@@ -4,6 +4,8 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    // only in the constructor function do we manipulate state like this.
+    // everywhere else, we will use a method called this.setState();
     this.state = { term: '' };
   }
 
@@ -12,7 +14,8 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)} />
+          onChange={event => this.onInputChange(event.target.value)} 
+        />
       </div>
     );
   }
