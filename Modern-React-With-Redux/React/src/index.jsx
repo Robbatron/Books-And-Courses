6164f7be17bb114b549f19a54872ff3810c47824
Robbie Delfs <robertdelfs@Roberts-MacBook-Pro.local>
2016-09-20 18:36:02 -0700

    changes?

---
+++ Modern-React-With-Redux/React/src/index.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDa3ZGvEqiKB2x0SFTMUUdcrmJSbiLMGZk';
+
+class App extends Component { 
+	constructor(props) {
+		super(props);
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+	  };
+		
+		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
