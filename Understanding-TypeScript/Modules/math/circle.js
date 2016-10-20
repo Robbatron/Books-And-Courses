70953cc23b2b16b55278d3bcab83f5afb6009790
Robbie Delfs <robertdelfs@Roberts-MacBook-Pro.local>
2016-10-19 22:45:11 -0700

    modules and interfaces

---
+++ Understanding-TypeScript/Modules/math/circle.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.PI = 3.14;
+function calculateCirumcumference(diameter) {
+    return diameter * exports.PI;
+}
+exports.calculateCirumcumference = calculateCirumcumference;
---
+++ Understanding-TypeScript/Modules/math/rectangle.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculateRectangle(width, length) {
+    return width * length;
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = calculateRectangle;
