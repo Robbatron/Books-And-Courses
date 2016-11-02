e420039d06809accab4773eadafbdcdbf7269f9f
Robbie Delfs <robertdelfs@Roberts-MacBook-Pro.local>
2016-11-01 22:44:31 -0700

    Decorators

--- Understanding-TypeScript/Decorators/app.js
+++ Understanding-TypeScript/Decorators/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // our decorator starts out as a normal function
 function logged(constructorFn) {
     console.log(constructorFn);
@@ -45,3 +48,61 @@ Plant = __decorate([
 ], Plant);
 const plant = new Plant();
 plant.print();
+// Method Decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+// Property Decorator
+function overwritable(value) {
+    return function (target, propName) {
+        const newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+class Project {
+    constructor(name) {
+        this.projectName = name;
+    }
+    calcBudget() {
+        console.log(1000);
+    }
+}
+__decorate([
+    editable(false)
+], Project.prototype, "calcBudget", null);
+const project = new Project("Super Project");
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
+console.log(project);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
+}
+class Course {
+    constructor(name) {
+        this.name = name;
+    }
+    printStudentNumbers(mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    }
+}
+__decorate([
+    __param(1, printInfo)
+], Course.prototype, "printStudentNumbers", null);
+const course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
--- Understanding-TypeScript/Decorators/app.ts
+++ Understanding-TypeScript/Decorators/app.ts
@@ -41,3 +41,67 @@ class Plant {
 
 const plant = new Plant();
 (<any>plant).print();
+
+// Method Decorator
+function editable(value: boolean) {
+    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
+    }
+}
+
+// Property Decorator
+function overwritable(value: boolean) {
+    return function(target: any, propName: string): any {
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    }
+}
+
+class Project {
+    // @overwritable(false)
+    projectName: string;
+
+    constructor(name: string) {
+        this.projectName = name;
+    }
+
+    @editable(false)
+    calcBudget() {
+        console.log(1000);
+    }
+}
+
+const project = new Project("Super Project");
+project.calcBudget();
+project.calcBudget = function() {
+    console.log(2000);
+}
+project.calcBudget();
+console.log(project);
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);        
+}
+
+class Course {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+        if (printAll) {
+            console.log(10000);
+        } else {
+            console.log(2000);
+        }
+    }
+}
+
+const course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
