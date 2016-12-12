ff22006e89b49bac2ddffedda20cdff41b54b7c4
Robbie Delfs <robbie@iMac.local>
2016-12-11 17:48:29 -0700

    Spread & Rest, Array Improvements, and Promises.

--- es6.io-master/06 - Iterables & Looping/memetext.js
+++ es6.io-master/06 - Iterables & Looping/memetext.js
@@ -1,16 +1,25 @@
-// const meme = text =>
-// 	[...text.toUpperCase()].reduce((prev, curr, i, ary) =>
-// 		[`${prev[0]}${ary[i]}  `, `${prev[1]}\n${ary[i + 1] || ''}  `]
-// 		, ['', '']).reduce((prev, curr) => prev + curr);
-
-const meme = t =>
-	[
-		[...t.toUpperCase()]
-			.join(' '), ...t
-				.toUpperCase()
-				.substr(1)
-	]
-		.join('\n');
+const meme = text =>
+	[...text.toUpperCase()]
+	.reduce((accumulator, current, index, array) =>
+		[
+			`${accumulator[0]}${array[index]} `, 
+			`${accumulator[1]}\n${array[index + 1] || ''}`
+		], ['', ''])
+		.reduce((accumulator, current) => accumulator + current);
 
 const result = meme('Aesthetics');
 console.log(result);
+
+
+
+
+
+
+// const meme = t =>
+// 	[
+// 		[...t.toUpperCase()]
+// 			.join(' '), ...t
+// 				.toUpperCase()
+// 				.substr(1)
+// 	]
+// 		.join('\n');
