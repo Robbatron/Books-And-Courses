cbaf3924c5bb584ca332977342f4960a648d8591
Robbie Delfs <robertdelfs@Roberts-MacBook-Pro.local>
2017-04-30 20:46:20 -0700

    stuff and things

---
+++ Lpthw/exe44a.py
@@ -0,0 +1,15 @@
+class Parent(object):
+
+    def implicit(self):
+	print 'PARENT implicit()'
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+
---
+++ Lpthw/exe44b.py
@@ -0,0 +1,13 @@
+class Parent(object):
+    def override(self):
+	print 'PARENT override()'
+
+class Child(Parent):
+    def override(self):
+	print 'CHILD override()'
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
