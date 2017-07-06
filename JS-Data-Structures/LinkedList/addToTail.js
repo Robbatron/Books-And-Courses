9ebb8e6f2a3b849a02a81857698ebfa2db72f104
Robbie Delfs <robbie@iMac.local>
2017-07-05 23:12:23 -0700

    LL comments

--- JS-Data-Structures/LinkedList/addToTail.js
+++ JS-Data-Structures/LinkedList/addToTail.js
@@ -1,9 +1,11 @@
 const { LinkedList, Node } = require('./LinkedList');
 
 // ADD A NODE TO THE TAIL OF THE LL
+
+// null: since this node will be added to the tail, we know that there will not be a nextNode in the LL
+// this.tail: previous parameter
+
 LinkedList.prototype.addToTail = function (value) {
-  // null: since this node will be added to the tail, we know that there will not be a nextNode in the LL
-  // this.tail: previous parameter
   let newNode = new Node(value, null, this.tail);
   if (this.tail) this.tail.next = newNode; // if LL is not empty... 
   else this.head = newNode; // if LL is empty, then set HEAD pointer equal to newNode (will be both HEAD and TAIL node)
