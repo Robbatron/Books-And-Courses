8f33614019998ffae0a27a5b0fd2057424bf1cc8
Robbie <robbie.delfs@gmail.com>
2018-02-21 16:39:10 -0700

    HAAAAAANNDDSSS

--- AlgoCasts/exercises/fizzbuzz/index.js
+++ AlgoCasts/exercises/fizzbuzz/index.js
@@ -4,17 +4,17 @@
 // and five print “fizzbuzz”. --- Example   fizzBuzz(5);   1   2   fizz   4 buzz
 
 function fizzBuzz(n) {
-    for (var i = 0; i < n; i++) {
-        if (i % 15 === 0) {
-            console.log("fizzbuzz");
-        } else if (i % 3 === 0) {
-            console.log("fizz");
-        } else if (i % 5 === 0) {
-            console.log("buzz");
-        } else {
-            console.log(i);
-        }
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
     }
+  }
 }
 
 module.exports = fizzBuzz;
--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
