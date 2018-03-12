b03a8bf0d753191b72f28b0ad8620072346db47a
Robbie Delfs <robbatron@gmail.com>
2018-03-11 22:36:25 -0700

    beep boop

---
+++ React-For-Beginners/catch-of-the-day/src/index.js
@@ -0,0 +1,7 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import './css/style.css';
+
+render(<StorePicker />, document.querySelector("#main"));
