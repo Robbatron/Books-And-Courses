d5ce325fdd470c3b42fc148cc8773ab42d08835e
Robbie Delfs <robbatron@gmail.com>
2018-03-14 00:59:55 -0700

    progress

--- React-For-Beginners/catch-of-the-day/src/components/App.jsx
+++ React-For-Beginners/catch-of-the-day/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
   state = {
@@ -16,14 +18,27 @@ class App extends Component {
     // 3. Set the new fishes object to state
     this.setState({ fishes });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" cool={false} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]}>
+                {key}
+              </Fish>
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- React-For-Beginners/catch-of-the-day/src/components/Header.jsx
+++ React-For-Beginners/catch-of-the-day/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 // stateless functional component
 const Header = ({ tagline, cool }) => (
