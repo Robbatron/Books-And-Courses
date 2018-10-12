315bec595308da3e267e71a1fa2bc3ed7170dbd5
Robbie Delfs <robbatron@gmail.com>
2018-10-12 16:32:53 -0700

    the ocean's no place for a squirrel

--- Advanced-React/sick-fits/backend/src/resolvers/Mutation.js
+++ Advanced-React/sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  }
+};
+
+module.exports = Mutations;
--- Advanced-React/sick-fits/backend/src/resolvers/Query.js
+++ Advanced-React/sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db")
+  // ^ use when just pushing and pulling data to db. custom logic (below) is not needed if you don't need checks/authentication
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
