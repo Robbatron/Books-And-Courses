d9dce0becb6c8ecc6fb6dbf4d8295119d907eca0
Robbie Delfs <robbatron@gmail.com>
2018-10-13 18:08:28 -0700

    Item component

--- Advanced-React/sick-fits/frontend/components/Items.js
+++ Advanced-React/sick-fits/frontend/components/Items.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,6 +17,18 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
 export default class Items extends Component {
   render() {
     return (
@@ -22,8 +36,15 @@ export default class Items extends Component {
         <p>Items!</p>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
-            console.log("payload:", data);
-            return <p>I found {data.items.length} items</p>;
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
       </div>
